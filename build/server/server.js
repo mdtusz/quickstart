2d1778099e601850de56b5319ff778059b246489
Miklós Tusz <mdtusz@gmail.com>
2017-12-14 13:39:05 -0500

    Add flow and server setup

---
+++ build/server/server.js
@@ -0,0 +1,79 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!*****************************!*\
+  !*** ./src/server/index.js ***!
+  \*****************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nfunction square(n) {\n  return n * n;\n}\n\nsquare(2);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9zcmMvc2VydmVyL2luZGV4LmpzPzZlMGQiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gQGZsb3dcbmZ1bmN0aW9uIHNxdWFyZShuOiBudW1iZXIpOiBudW1iZXIge1xuICByZXR1cm4gbiAqIG47XG59XG5cbnNxdWFyZSgyKTtcblxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyBzcmMvc2VydmVyL2luZGV4LmpzIl0sIm1hcHBpbmdzIjoiOztBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///0\n");
+
+/***/ })
+/******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 const SRC_PATH = path.join(__dirname, 'src');
 const BUILD_PATH = path.join(__dirname, 'build');
 
-const js = {
-  entry: path.join(SRC_PATH, 'js', 'index.js'),
+const client = {
+  target: 'web',
+  entry: path.join(SRC_PATH, 'client', 'index.js'),
   output: {
     filename: 'bundle.js',
     path: path.join(BUILD_PATH, 'js')
@@ -16,14 +18,15 @@ const js = {
   },
   resolve: {
     alias: {
-      _: path.join(SRC_PATH, 'js')
+      _: path.join(SRC_PATH, 'client')
     }
   },
+  plugins: [new FlowBabelWebpackPlugin()],
   devtool: 'source-map'
 };
 
 const scss = {
-  entry: path.join(SRC_PATH, 'scss/index.scss'),
+  entry: path.join(SRC_PATH, 'scss', 'index.scss'),
   output: {
     filename: 'style.css',
     path: path.join(BUILD_PATH, 'css')
@@ -48,4 +51,28 @@ const scss = {
   plugins: [new ExtractTextPlugin('style.css')]
 };
 
-module.exports = [js, scss];
+const server = {
+  target: 'node',
+  entry: path.join(SRC_PATH, 'server', 'index.js'),
+  output: {
+    path: path.join(BUILD_PATH, 'server'),
+    filename: 'server.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      _: path.join(SRC_PATH, 'server')
+    }
+  },
+  plugins: [new FlowBabelWebpackPlugin()]
+};
+
+module.exports = [client, scss, server];
